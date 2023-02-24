3a11dc418fbfd2b699cacd628104712ae870c7e6
Kera-Parham <124192401+Kera-Parham@users.noreply.github.com>
2023-02-24 08:17:33 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("March 31, 2023 15:37:25").getTime();
+var countDownDate = new Date("March 31, 2023 08:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
